ea6a8d9541ca1d13a2ac3bd7c4fe0b168140f947
MoazamAli45 <21-SE-48@students.uettaxila.edu.pk>
2023-09-14 17:34:18 +0500

    Files Updated

--- Backend/Routes/productRoutes.js
+++ Backend/Routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../Controllers/authController");
 
 const productController = require("../Controllers/productController");
-// router.use(authController.protect);
+router.use(authController.protect);
 router.get("/", productController.getAllProducts);
 //  middleware
 router.get("/:productId", productController.getProductById);
