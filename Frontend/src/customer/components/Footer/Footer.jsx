02b93be9be8c1fbae1466ddadc598adad2a41e96
MoazamAli45 <21-SE-48@students.uettaxila.edu.pk>
2023-09-14 22:22:09 +0500

    Files Updated

--- Frontend/src/customer/components/Footer/Footer.jsx
+++ Frontend/src/customer/components/Footer/Footer.jsx
@@ -147,7 +147,7 @@ const Footer = () => {
                 />
               </div>
               <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-primary hover:text-[#111827] rounded">
-                Button
+                Submit
               </button>
             </div>
             <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
